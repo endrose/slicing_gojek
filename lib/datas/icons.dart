a3aef527a20e380ca3db1098bbb01a5a1a1a5538
endrose <endros007@gmail.com>
2024-07-28 22:51:06 +0700

    first slicing

---
+++ lib/datas/icons.dart
@@ -0,0 +1,26 @@
+import 'package:slicing_gojek/models/gojek_icon.dart';
+import 'package:slicing_gojek/theme.dart';
+
+List<GojekIcon> icons = [
+  GojekIcon(icon: 'pay', title: 'Baya'),
+  GojekIcon(
+    icon: 'topup',
+    title: 'topup',
+  ),
+  GojekIcon(icon: 'eksplore', title: 'Eksplore')
+];
+
+List<GojekIcon> menuIcons = [
+  GojekIcon(icon: 'goride', title: 'GoRide', color: green2),
+  GojekIcon(icon: 'gocar', title: 'GoCar', color: green2),
+  GojekIcon(icon: 'gofood', title: 'GoFood', color: red),
+  GojekIcon(
+    icon: 'gosend',
+    title: 'GoSend',
+    color: green2,
+  ),
+  GojekIcon(icon: 'gomart', title: 'GoMart', color: red),
+  GojekIcon(icon: 'gopulsa', title: 'GoPulsa', color: blue2),
+  GojekIcon(icon: 'goclub', title: 'GoClub', color: purple),
+  GojekIcon(icon: 'lainnya', title: 'Lainnya', color: dark4)
+];
---
+++ lib/models/gojek_icon.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class GojekIcon {
+  //
+  final String icon;
+  final String title;
+  final Color? color;
+
+  GojekIcon({required this.icon, required this.title, this.color});
+}
